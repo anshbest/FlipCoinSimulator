5e599b0695d5136ca95d1a913bdc1e43ccc352e8
anshbest <anshulmnc123@gmail.com>
2020-08-09 06:29:59 +0530

    each head or tail needs to be traversed first and winner wins by difference of 2

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
-H=0
-T=0
+Heads=0
+Tails=0
 function tosses(){
 IsChoice=$((RANDOM%2))
 if [ $IsChoice -eq 0 ]
 then
-	((H++))
+        ((Heads++))
 else
-	((T++))
+        ((Tails++))
 fi
 }
 
-for (( i=0 ;i<=40; i++ ))                                     
-do
-  # function call
+for (( i=0 ;i<=40; i++ ))
+   do
       tosses
+           while [ $Heads  -lt 21 ]
+            do
 
-	if [ $(($H-$T)) -ge 2 ]
-	then
-		echo "HURRAY H WINNER"
-		break
-	elif [ $(($T-$H)) -ge 2 ]
-	then
-		echo "HURRAY T WINNER"
-		break
-       elif [ $H -eq 20 ] && [ $T -eq 20 ]
-	then
-		 echo "ITS a TIE"
-		break
-	fi
-done
- 
+        if [ $(($Heads-$Tails)) -ge 2 ]
+        then
+                echo "HURRAY HEADS WINNER"
+                break
+            fi
+          done
+           while [ $Tails  -lt 21 ]
+               do
+
+             if [ $(($Tails-$Heads)) -ge 2 ]
+               then
+                echo "HURRAY TAILS WINNER"
+                break
+                fi
+              done
+
+ if [ $Heads -eq 20 ] && [ $Tails -eq 20 ]
+                then
+                 echo "ITS a TIE"
+                break
+               fi
+
+done 
