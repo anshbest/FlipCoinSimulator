1df37ce1a4f79b88c74e5984df5a954823390304
anshbest <anshulmnc123@gmail.com>
2020-08-09 06:02:41 +0530

    head win or tail win or a tie for coin tosses

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -2,7 +2,7 @@
 
 H=0
 T=0
-for (( i=0 ; i<20 ; i++ ))
+for (( i=0 ; i<=40; i++ ))
 do
 	IsChoice=$((RANDOM%2))
 	if [ $IsChoice -eq 0 ]
@@ -11,5 +11,17 @@ do
 	else
 		((T++))
 	fi
+	if [ $H -eq 21 ]
+	then
+		echo "H win by $(($H-$T))"
+		break
+	elif [ $T -eq 21 ]
+	then
+		echo "T win by $(($T-$H))"
+		break
+	elif [ $H -eq 20 ] && [ $T -eq 20 ]
+	then
+		echo "Tie"
+		break
+	fi
 done
-echo "H occured $H times and T occured $T times"
